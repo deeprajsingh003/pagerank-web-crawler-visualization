dcd62671abc158ab9baaacda1aeb3a085c161b49
Deepraj Singh <deeprajpremsingh@gmail.com>
2026-03-17 17:40:41 +0530

    Update sprank.py

--- sprank.py
+++ sprank.py
@@ -8,7 +8,7 @@ conn = sqlite3.connect('spider.sqlite')
 cur = conn.cursor()
 
 # Find the ids that send out page rank - we only are interested
-# in pages in the SCC that have in and out links
+# in pages in the SCC that have in & out links
 cur.execute('''SELECT DISTINCT from_id FROM Links''')
 from_ids = list()
 for row in cur: 
